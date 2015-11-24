82f240c6f5a6ef059429587a6ee722eaf62c38cd
Hamish <hamish.brown@plantandfoodco.nz>
2015-11-25 09:14:37 +1300

    Added System.Data

--- Utilities/ReflectionUtilities.cs
+++ Utilities/ReflectionUtilities.cs
@@ -333,6 +333,7 @@ namespace APSIM.Shared.Utilities
                         Params.ReferencedAssemblies.Add("System.dll");
                         Params.ReferencedAssemblies.Add("System.Xml.dll");
                         Params.ReferencedAssemblies.Add("System.Windows.Forms.dll");
+                        Params.ReferencedAssemblies.Add("System.Data.dll");
                         //Params.ReferencedAssemblies.Add("APSIM.Shared.dll");
                         Params.ReferencedAssemblies.Add(System.IO.Path.Combine(Assembly.GetExecutingAssembly().Location));
 
