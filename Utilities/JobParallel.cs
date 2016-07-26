c25879b2b07ed0cd1a2f33a71efe91dac51c9e51
Dean Holzworth <Dean Holzworth>
2016-07-26 14:57:45 +1000

    Revert "Reworked JobManager to make it simpler."
    This reverts commit 61905621c7a7059af963b984105554db169fcef7.

--- Utilities/JobParallel.cs
+++ Utilities/JobParallel.cs
@@ -5,31 +5,68 @@
 //-----------------------------------------------------------------------
 namespace APSIM.Shared.Utilities
 {
+    using System;
     using System.Collections.Generic;
-    using System.ComponentModel;
+    using System.Linq;
+    using System.Text;
+    using System.Threading;
+
     /// <summary>
     /// A composite class for a sequence of jobs that will be run asynchronously.
     /// If an error occurs in any job, then this job will also produce an error.
     /// </summary>
     public class JobParallel : JobManager.IRunnable
     {
+        /// <summary>Gets a value indicating whether this instance is computationally time consuming.</summary>
+        public bool IsComputationallyTimeConsuming { get { return false; } }
+
         /// <summary>A list of jobs that will be run in sequence.</summary>
         public List<JobManager.IRunnable> Jobs { get; set; }
+        
+        /// <summary>Gets a value indicating whether this job is completed. Set by JobManager.</summary>
+        public bool IsCompleted { get; set; }
 
-        /// <summary>Constructor</summary>
-        public JobParallel()
-        {
-            Jobs = new List<JobManager.IRunnable>();
-        }
-
+        /// <summary>Gets the error message. Can be null if no error. Set by JobManager.</summary>
+        public string ErrorMessage { get; set; }
+        
         /// <summary>Called to start the job.</summary>
-        /// <param name="jobManager">The job manager running this job.</param>
-        /// <param name="workerThread">The thread this job is running on.</param>
-        public void Run(JobManager jobManager, BackgroundWorker workerThread)
+        /// <param name="sender">Event sender</param>
+        /// <param name="e">Event arguments</param>
+        public void Run(object sender, System.ComponentModel.DoWorkEventArgs e)
         {
+            // Get a reference to the JobManager so that we can add jobs to it.
+            JobManager jobManager = e.Argument as JobManager;
+
             // Add all jobs to the queue
             foreach (JobManager.IRunnable job in Jobs)
-                jobManager.AddChildJob(this, job);
+                jobManager.AddJob(job);
+
+            // Wait for it to be completed.
+            while (!AllCompleted)
+                Thread.Sleep(200);
+
+            // Get a possible error message. Null if no error.
+            string ErrorMessage = string.Empty;
+            foreach (JobManager.IRunnable job in Jobs)
+                ErrorMessage += job.ErrorMessage;
+            if (ErrorMessage != string.Empty)
+                throw new Exception(ErrorMessage);
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether all jobs are completed.
+        /// </summary>
+        /// <value><c>true</c> if completed; otherwise, <c>false</c>.</value>
+        private bool AllCompleted
+        {
+            get
+            {
+                foreach (JobManager.IRunnable job in Jobs)
+                    if (!job.IsCompleted)
+                        return false;
+
+                return true;
+            }
         }
 
     }
